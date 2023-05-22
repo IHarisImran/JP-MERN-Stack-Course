91907d720912b0a5340b0fc309f8296345a084f4
Muhammad Haris <iharisimran@gmail.com>
2023-05-22 18:30:50 +0500

    assignment 2 init v2

---
+++ jp-assignments/js/assignment 2/app.js
@@ -0,0 +1,589 @@
+//chapter 21 Q1
+
+//var  allLower = userInput.toLowercase();
+//chapter 21 Q2
+
+//var a ="ALI AHMED SIDDIQUI";
+//var a= a.toLowerCase(); 
+//console.log(a);
+
+//chapter 21 Q3
+//var a ="ali ahmed siddiqui";
+//var a= a.toUpperCase(); 
+//console.log(a);
+
+
+//chapter 21 Q4
+//var a ="ALI AHMED SIDDIQUI";
+//var b= a.toLowerCase(); 
+//console.log(b);
+
+
+
+//chapter 21 Q5
+
+// var arr = ["ALI","HELLO","HOW","ARE","YOUR","BOYS"];
+// var a = arr[0].toLowerCase();
+// console.log(a);
+
+ //chapter 21 Q6
+
+
+// var a = "hello world";
+// var b = a.toUpperCase();
+// alert(b);
+
+//chapter 21 Q7
+
+// var city = "kaRaChI";
+// var a = city.slice(0,1).toUpperCase() +city.slice(1).toLowerCase(); 
+//  console.log(a);
+
+
+//chapter 22-25 Q1
+
+// var sameWords = "captain";
+// var b = sameWords.slice(1,3);
+// console.log(b);
+
+
+
+//chapter 22-25 Q2
+
+// var a = "khfhghsdgfsjdgfsjdf";
+// var b = a.length;
+// console.log(b);
+
+
+//chapter 22-25 Q3
+
+// var animal = "elephant";
+// var seg = animal.slice(2,6);
+// console.log(seg);
+
+
+//chapter 22-25 Q4
+
+// var a = "khfhghsdgfsjdgfsjdf";
+// var b = a.length;
+// console.log(b);
+
+
+
+
+//chapter 22-25 Q5
+
+// var a = "ali is a goodboy";
+// var b = a.length;
+// console.log(b);
+
+// var c = a.slice(1,13);
+// console.log(c);
+
+
+//chapter 22-25 Q6
+
+//  var text = "To be or not to be";
+//  var indx =  text.indexOf("be");
+
+//  console.log(indx);
+
+
+//chapter 22-25 Q7
+
+// var text = "To be or not to be";
+// var indx =  text.lastIndexOf("be");
+
+// console.log(indx);
+
+
+//chapter 22-25 Q8
+
+// var text = "To go or not to go";
+// var indx = text.lastIndexOf("go");
+// console.log(indx);
+//chapter 22-25 Q9
+
+// if(indxNum == indx);
+
+//chapter 22-25 Q10
+
+// var a = "abcde";
+// var b = a.charAt(2);
+// console.log(b);
+
+//chapter 22-25 Q11
+
+// var text = "ieufuhiwehfieuhfiuewhfw";
+// var chr = text.slice(1,10);
+// console.log(chr);
+
+//chapter 22-25 Q12
+
+// var str = "eeeefefwefsdcdcwefvfvjhk";
+// var ch = str.charAt(23);
+// console.log(ch);
+
+
+//chapter 22-25 Q13
+
+// var a = prompt("enter a any string");
+// var b = a.charAt(4); 
+// console.log(b);
+
+
+//chapter 22-25 Q14
+ 
+// var a = "hello world";
+// var b = a.charAt(4);
+
+// if(b = "o"){
+//     console.log("wow");
+// }
+
+
+//chapter 22-25 Q15
+//  var a = "jhjsdahbcjhbdsc";
+// for(var i=0;i<=a.length;i++){
+//         var  b = a.charAt(i);
+
+
+//         console.log(b);
+        
+//  }
+
+ //chapter 22-25 Q16
+
+// var x = "one";
+// var y = x.replace("1");
+
+// console.log(y);
+
+ //chapter 22-25 Q17
+
+// var x = "yes";
+// var y = x.replace("no");
+
+// console.log(y);
+//chapter 22-25 Q18
+// var x = "ehg";
+// var y = x.replace("a","z");
+
+
+// console.log(y);
+
+ //chapter 26 Q1
+
+// var a = 34.0897;
+// var b = Math.round(a);
+// console.log(b);
+
+// //chapter 26 Q2
+//  var orignum = 34.0897;
+// var roundedNum = Math.ceil(orignum);
+// console.log(roundedNum);
+
+
+
+// //chapter 26 Q3
+// var orignum = 34.0897;
+// var roundedNum = Math.(orignum);
+// console.log(roundedNum);
+
+
+//chapter 26 Q4
+// var b = 3.4567 ;
+// var a = Math.round(b);
+// console.log(a);
+
+//chapter 26 Q5
+
+// var a = 0.5;
+// var b = Math.ceil(a);
+// console.log(b);
+
+
+//chapter 27 Q1
+
+// var a = Math.random();
+// var b = a*50;
+// console.log(b);
+
+//chapter 27 Q2
+
+// var a = Math.random();
+// var b = a*10;
+// var c = Math.round(b);
+// console.log(c);
+
+
+//chapter 27 Q3
+//  var a = Math.random();
+//  var b = a*6;
+//  var c = Math.round(b);
+//  console.log(c);
+
+ 
+//chapter 27 Q4
+
+//  var a = Math.random();
+//   var b = a*2;
+//   var c = Math.round(b);
+//  console.log(c);
+
+ 
+//chapter 28-29 Q1
+
+//+
+//parseInt
+//number
+
+//chapter 28-29 Q2
+
+// var a = "123";
+// var b = parseInt(a) ;
+// console.log(b);
+
+//chapter 28-29 Q3
+
+//  var a = "123.8767";
+//  var b = parseFloat(a) ;
+//  console.log(b);
+
+
+//chapter 28-29 Q4
+
+//using === 
+
+//chapter 28-29 Q5
+
+// var a = 654.34;
+// var b = a.toString();
+// console.log(b);
+
+//chapter 28-29 Q6
+
+// var a = 42;
+// var b = a.toString();
+// console.log(b);
+
+//chapter 28-29 Q7
+
+// var a = "3.876"
+// var b = parseInt(a);
+// console.log(b);
+
+//chapter 30 Q1
+
+// var a = Math.random();
+// var  b = a*10;
+// var c = b.toFixed(4);
+// var d = c.toString();
+// console.log(b);
+
+
+
+//chapter 30 Q2
+
+// var a = Math.random();
+// var  b = a*10;
+// var c = b.toFixed(2);
+// var d = c.toString();
+// console.log(b);
+
+
+
+
+
+//chapter 30 Q3
+ 
+// var num = 14.5656;
+// var b = Math.random();
+// var c = b*100;
+// var a =  Math.round(c.toFixed(4))
+// if(num == a)  
+ 
+
+//chapter 30 Q4
+
+
+//  var a = Math.random();
+//  var c = a.toFixed(2);
+// var d = c.toString();
+//  alert(d);
+
+//chapter 31-34 Q1
+
+// var dobj = new Date();
+// console.log(dobj);
+
+
+//chapter 31-34 Q2
+
+//  var a = new Date();
+
+// var str = a.toString();
+//  console.log(str);
+
+
+
+//chapter 31-34 Q3
+
+// var date = new Date();
+
+// var d = date.getDay();
+// console.log(d+1);
+
+//chapter 31-34 Q4
+
+// var date = new Date();
+// var d = date.getDay();
+// var arr = ["sunday","monday","tuesday","wednesday","thursday","firday","saturday","sunday"];
+
+// var e = arr[d];
+// console.log(e);
+
+//chapter 31-34 Q5
+
+
+// var dobj = new Date();
+// console.log(dobj);
+
+//chapter 31-34 Q6
+
+// var a = new Date("dec31,2020"); 
+// console.log(a);
+//chapter 31-34 Q7
+
+
+// var a = new Date("feb 2,1992"); 
+// console.log(a);
+
+//chapter 31-34 Q8
+
+// var date = new Date();
+// var a = date.getTime();
+// console.log(a); 
+
+//chapter 31-34 Q9
+
+// var date = new Date();
+// var a = date.setFullYear("2020");
+//  console.log(date); 
+
+
+//chapter 31-34 Q10
+
+// var date = new Date();
+// var a = date.setMonth("jan");
+//  console.log(date); 
+
+
+//chapter 31-34 Q11
+
+// var date = new Date();
+// var a = date.setDay("mon");
+//  console.log(date); 
+
+
+//chapter 31-34 Q12
+
+//  var date = new Date();
+//  var b = +prompt("enter min");
+//  var a = date.setMinutes(b);
+//  console.log(date); 
+
+
+//chapter 31-34 Q13
+
+
+// var date = new Date();
+// var a = date.setHours("3");
+//  console.log(date); 
+
+
+
+//chapter 31-34 Q14
+
+// date = new Date();
+// var a = date.getTime();
+
+// date2 = new Date("nov 4, 2004");
+// var b = date2.getTime();
+
+// console.log(a);
+// console.log(b);
+
+// var total = (a-b)/(1000*60*60*24*365); 
+// console.log(total);
+// d =  Math.floor(total);
+// console.log(d);
+
+
+//chapter 35-37 Q1
+
+// alert("function a(){}");
+
+//chapter 35-37 Q2
+
+// function name(nam){
+//     console.log(nam + " is nice name");
+// }
+// var b = prompt("enter your name");
+//  name(b); 
+
+//chapter 35-37 Q3
+
+
+// function h(){
+//     console.log("hello");
+// }
+
+
+// function w(){
+//     console.log("world");
+// }
+
+// function t(h , w){
+//     console .log(h+w);
+// }
+// t(h ,w)
+
+//chapter 35-37 Q4
+
+// function name(a){
+ 
+
+//     alert(a); 
+// }
+
+// var a = prompt("enter a name");
+// name(a)
+
+
+//chapter 35-37 Q5
+
+// function a(str,num,chr){
+//     console.log(str+num+chr);
+// } 
+// a("ert",3,'G');
+
+//chapter 35-37 Q6
+
+
+// function a(str,str2){
+//     console.log(str+str2);
+// } 
+// a("ali","ahmed");
+
+
+//chapter 35-37 Q7
+
+// function mul(num1,num2,num3){
+// var d = num1 * num2 * num3;
+// console.log(d);
+// }
+
+// mul(56,78,90)
+
+//chapter 35-37 Q8
+
+
+
+
+//chapter 35-37 Q9
+
+
+// function mul(num1,num2){
+// var d = num1 + num2 ;
+// console.log(d);
+// }
+
+// mul(56,78);
+
+
+//chapter 35-37 Q10
+
+
+
+//chapter 35-37 Q11
+
+// function a (){
+//  var b = 6+6+6;
+//      return b;
+
+// }
+// console.log(b);
+
+
+//chapter 35-37 Q12
+
+
+
+//chapter 35-37 Q13
+//chapter 35-37 Q14
+//chapter 35-37 Q15
+//chapter 35-37 Q16
+//chapter 35-37 Q17
+
+
+//chapter 39-40 Q1
+// var num1 = +prompt("enter num1");
+// var num2 = +prompt("enter num2");
+// var operator = +prompt("enter a opertor");
+// var b;
+// switch(operator){
+
+// case '+':
+//      b = num1 + num2;
+//     alert(b);
+//     break;
+
+//  case '-':
+//         var b = num1 -num2;
+//         alert(b);
+
+//         break;
+    
+
+//  case '*':
+//             var b = num1 *num2;
+//             alert(b);
+
+//             break;
+
+            
+            
+// case '/':
+//     var b = num1 /num2;
+//     alert(b);
+
+//     break;
+
+// // }
+// // console.log(b);
+// //chapter 39-40 Q2
+// var days = prompt("enter a day");
+// switch(days) {
+//     case "mon":
+//     alert("today is monday");
+//     break;
+// case "tue":
+//     alert("today is tuesday");
+//     break; 
+// case "wed":
+//     alert("today is wednesday");
+//     break;
+// case "thur":
+//     alert("today is thursday");
+//     break;
+// case "fri":
+//     alert("today is friday");
+//     break;
+// case "sat":
+//     alert("today is saturday");
+//     break;
+// case "sun":
+//     alert("today is sunday");
+//     break;
+// }
