2f94716437d33e9ab495a6ce9f8ded3a098ebdff
Muhammad Haris <iharisimran@gmail.com>
2023-06-20 21:40:32 +0500

    added database

---
+++ public/firebase-sdk.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase, set, push, ref, onValue, remove } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+initializeApp({
+    apiKey: "AIzaSyAu8GXqdif0BoSX-yVgRp7LgWtPYGpQrHA",
+    authDomain: "jp-mern-stack-course.firebaseapp.com",
+    projectId: "jp-mern-stack-course",
+    storageBucket: "jp-mern-stack-course.appspot.com",
+    messagingSenderId: "215485518377",
+    appId: "1:215485518377:web:f803a41063bbca840a0238"
+});
+
+const database = getDatabase();
+
+async function getValue(param) {
+    return new Promise((res, rej) => {
+        onValue(ref(database, param), (data) => {
+            res(Object.values(data.val()))
+        }, (error) => { rej(error) })
+    })
+};
+
+export { database, set, push, ref, getValue, remove };
